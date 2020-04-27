a88f93641d0fca92c8725736c7c2f6016513b0e8
Marius Banea <marius@w-d-s.co.uk>
2020-04-27 17:29:45 +0100

    commenting the book

--- src/Book.js
+++ src/Book.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 class Book extends React.Component {
     render() {
+        //get the arrays of authors
         const authors = this.props.author
+        //get the saleability status
         const saleability = this.props.saleability
 
+        //if the book is free, there is no price to show
         if (saleability === 'FREE') {
             return (
                 <div>
@@ -24,6 +27,7 @@ class Book extends React.Component {
                 </div>
             )
         }
+        //if the book is NOT free, show the price details
         else {
             const price = this.props.price
             const priceTag = (price) ? <h4>Price: {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price.amount)}</h4> : false
